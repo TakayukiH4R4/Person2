ca93d50b3d721528933193ef2be6f8070719cdb9
TakayukiH4R4 <sthaikgaoytuok4i@gmail.com>
2024-11-27 21:49:08 +0900

    演習３，４，５を終了

--- src/jp/co/Person2/action/Test.java
+++ src/jp/co/Person2/action/Test.java
@@ -15,6 +15,19 @@ public class Test {
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
 
+		Person saburo = new Person("saburo");
+
+		System.out.println(saburo.name);
+		System.out.println(saburo.age);
+
+		Person nanashi = new Person(25);
+		System.out.println(nanashi.name);
+		System.out.println(nanashi.age);
+
+		Person hanako = new Person("hanako",17);
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+
 
 
 	}
