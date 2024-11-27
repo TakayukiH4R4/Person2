94c22d85f76a7b2cde5840ac84a7f818e425460a
TakayukiH4R4 <sthaikgaoytuok4i@gmail.com>
2024-11-27 21:30:05 +0900

    演習１を終了

--- src/jp/co/Person2/action/Person.java
+++ src/jp/co/Person2/action/Person.java
@@ -2,4 +2,7 @@ package jp.co.Person2.action;
 
 public class Person {
 
+	public String name = null;
+	public int age = 0;
+
 }
--- src/jp/co/Person2/action/Test.java
+++ src/jp/co/Person2/action/Test.java
@@ -2,4 +2,10 @@ package jp.co.Person2.action;
 
 public class Test {
 
+	public static void main(String[] args) {
+
+		Person taro = new Person();
+
+	}
+
 }
